339cf4e74164476de3480f0c46855b768d50c327
Vetorim <irigerd@gmail.com>
2023-07-03 23:20:42 -0500

    AI wandering state partically done

--- Assets/Script/Enemy/AI/WanderingState.cs
+++ Assets/Script/Enemy/AI/WanderingState.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class WanderingState : State
 {
-    PawnMoving moving;
+    //PawnMoving moving;
     Wandering wandering;
+
+    private float timer;
+    private Vector3 direction;
+
+    public float moveSpeed =2f;
 
 
     public WanderingState(GameObject go, StateMachine sm) : base(go, sm){
@@ -16,20 +21,39 @@ public class WanderingState : State
         base.Enter();
         //the gameobject is from State.cs, the line
         // gameobject =go;
-        moving = gameobject.GetComponent<PawnMoving>();
+        //moving = gameobject.GetComponent<PawnMoving>();
         wandering = gameobject.GetComponent<Wandering>();
-
+        timer =0f;
+        direction =GetRandomDirection();
     }
 
     public override void Update(){
         base.Update();
+        timer += Time.deltaTime;
+
+        if (timer >= 2f)
+        {
+            direction = GetRandomDirection();
+            timer = 0f;
+        }
+
+        gameobject.transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
 
     public override void FixedUpdate(){
         base.FixedUpdate();
+
     }
 
     public override void Exit(){
         base.Exit();
+    }
+
+    private Vector3 GetRandomDirection()
+    {
+        // Generate a random direction
+        float x = Random.Range(-1f, 1f);
+        float z = Random.Range(-1f, 1f);
+        return new Vector3(x, 0f, z).normalized;
     }
 }
